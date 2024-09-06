1bfce272dcf323f81d651e4c7c154daba98c1a2d
Kevin Rosales <kevin.rosales98@hotmail.com>
2024-09-06 13:00:36 -0600

    Se modifico la forma en que agregar las rutas

--- app/settings.py
+++ app/settings.py
@@ -3,6 +3,8 @@ from sqlalchemy import create_engine, Engine
 from sqlalchemy.ext.declarative import declarative_base
 from passlib.context import CryptContext
 from typing import List
+import importlib
+import inspect
 
 
 class Settings(BaseSettings):
@@ -20,6 +22,23 @@ class Settings(BaseSettings):
         
     ]
 
+    def routers(self, app):
+        for router_path in self.INSTALLED_ROUTERS:
+            module = importlib.import_module(router_path)
+            app.include_router(module.router)
+
+    def middlewares(self, app):
+        for middleware in self.MIDDLEWARES:
+            module = importlib.import_module(middleware)
+            for name in dir(module):
+                member = getattr(module, name)
+            
+            # Verificar si el miembro es una clase
+            if inspect.isclass(member):
+                print(f'Member name: {name}')
+
+                app.add_middleware(name)
+                
     class Config:
         env_file = ".env"
 
--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 import importlib
 import uvicorn 
-import inspect
 
 from app.settings import settings
 
@@ -11,20 +10,9 @@ app = FastAPI(
     debug=settings.DEBUG
 )
 
-for router_path in settings.INSTALLED_ROUTERS:
-    module = importlib.import_module(router_path)
-    app.include_router(module.router)
 
-for middleware in settings.MIDDLEWARES:
-    module = importlib.import_module(middleware)
-    for name in dir(module):
-        member = getattr(module, name)
-    
-    # Verificar si el miembro es una clase
-    if inspect.isclass(member):
-        print(f'Member name: {name}')
-
-        app.add_middleware(name)
+settings.routers(app)
+settings.middlewares(app)
 
 
 if __name__ == '__main__':
