079f3697dfc2209aeed803600d72643310cf8dfe
Kevin Rosales <kevin.rosales98@hotmail.com>
2024-09-10 21:08:01 -0600

    Se agregaron metodos GET, POST, PUT, DELETE al modulo de Notas

--- core/notes/routers.py
+++ core/notes/routers.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException, Path, status
 from fastapi.responses import JSONResponse, RedirectResponse
 
@@ -6,11 +7,30 @@ from app.database import get_db, Session
 from core.users.models import User 
 from core.auth.src import get_current_user
 
-from .schemas import NoteCreate
+from .schemas import NoteCreate, NoteUpdate, NoteSchema
 from .models import Note
 
 
 router = APIRouter()
+
+
+@router.get("/notes", tags=['Notes'], response_model=List[NoteSchema])
+async def get_notes(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    notes = db.query(Note).filter(Note.user_id == current_user.id)
+    if notes is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Notes not found")
+    return notes
+
+
+@router.get("/notes/{id}", tags=['Notes'], response_model=NoteSchema)
+async def get_notes(id: int,db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    note = db.query(Note).filter(Note.id == id and Note.user_id == current_user.id).first()
+
+    if note is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+    
+    return note
+
 
 @router.post("/notes", tags=['Notes'])
 async def create_note(note: NoteCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
@@ -23,3 +43,33 @@ async def create_note(note: NoteCreate, db: Session = Depends(get_db), current_u
     db.commit()
     db.refresh(new_note)
     return new_note
+
+@router.put('/notes/{id}', tags=['Notes'], response_model=NoteSchema)
+async def update_note(id: int, note_update: NoteUpdate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    note = db.query(Note).filter(Note.id == id, Note.user_id == current_user.id).first()
+
+    if note is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+
+    # Actualizar los campos solo si han sido proporcionados
+    if note_update.title is not None:
+        note.title = note_update.title
+    if note_update.description is not None:
+        note.description = note_update.description
+
+
+    db.commit()
+    db.refresh(note)
+    return note
+
+
+@router.delete('/notes/{id}', tags=['Notes'])
+async def update_note(id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    note = db.query(Note).filter(Note.id == id, Note.user_id == current_user.id).first()
+    
+    if note is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Note not found")
+    
+    db.delete(note)
+    db.commit()
+    return {"message": "Note deleted successfully"}
--- core/notes/schemas.py
+++ core/notes/schemas.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 class NoteBase(BaseModel):
@@ -6,9 +6,13 @@ class NoteBase(BaseModel):
     description: Optional[str] = None 
 
 class NoteCreate(NoteBase):
+    title: str = Field(min_length=5, max_length=100)
+    description: str = Field(min_length=5)
+
+class NoteUpdate(NoteBase):
     pass
 
-class Note(NoteBase):
+class NoteSchema(NoteBase):
     id: int
     user_id: int
 
